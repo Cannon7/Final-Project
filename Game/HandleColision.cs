c5c80ccfc364e9ed98b1a5bc2d2e3ea187311978
derekely <derekely21@gmail.com>
2022-11-29 16:14:15 -0700

    Copied stuff from corn to get game over stuff

--- Game/HandleColision.cs
+++ Game/HandleColision.cs
@@ -23,8 +23,9 @@ namespace Example.Colliding
             try
             {
                 // get the actors from the cast
-                Actor actor1 = scene.GetFirstActor("actor1");
-                Actor actor2 = scene.GetFirstActor("actor2");
+                Actor 
+                Actor bullet = scene.GetFirstActor("bullets");
+                Actor alien = scene.GetFirstActor("aliens");
                 
 
 
@@ -32,20 +33,54 @@ namespace Example.Colliding
                 /// THIS STUFF HERE IS FROM THE EXAMPLE CODE!!! WE'LL NEED TO UPDATE FROM HERE WHEN BULLETS AND ACTORS ARE MADE.
 
                 // detect a collision between the actors.
-                if (actor2.Overlaps(actor1))
+                if (actor2.Overlaps(actor1)) // If bullet hits actor
                 {
-                    // resolve by changing the actor's color to something else
-                    actor2.Tint(Color.Green());
+                    // Remove both actors
+                    Remove(actor2);
+                    Remove(actor1);
                 }
                 else
                 {
-                    // otherwise, just make it the original color
-                    actor2.Tint(Color.Red());
+                    // otherwise, remove at end of screen
+                    
                 }
             }
             catch (Exception exception)
             {
                 callback.OnError("Couldn't check if actors collide.", exception);
+            }
+        }
+        // From Corn
+        private void HandleGameOver(Cast cast)
+        {
+            if (_isGameOver == true)
+            {
+                Snake snake = (Snake)cast.GetFirstActor("p1");
+                Snake snake2 = (Snake)cast.GetFirstActor("p2");
+                List<Actor> segments = snake.GetSegments();
+                List<Actor> segments2 = snake2.GetSegments();
+                Food food = (Food)cast.GetFirstActor("food");
+
+                // create a "game over" message
+                int x = Constants.MAX_X / 2;
+                int y = Constants.MAX_Y / 2;
+                Point position = new Point(x, y);
+
+                Actor message = new Actor();
+                message.SetText("Game Over!");
+                message.SetPosition(position);
+                cast.AddActor("messages", message);
+
+                // make everything white
+                foreach (Actor segment in segments)
+                {
+                    segment.SetColor(Constants.WHITE);
+                }
+                foreach (Actor segment2 in segments2)
+                {
+                    segment2.SetColor(Constants.WHITE);
+                }
+                food.SetColor(Constants.WHITE);
             }
         }
     }
